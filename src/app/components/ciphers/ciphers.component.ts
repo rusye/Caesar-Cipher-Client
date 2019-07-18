129b2da1b9832e19e6109bb55ad9da73d0e261fb
Russ yev <ruslanyev@yahoo.com>
2019-07-17 23:47:08 -0700

    displaying cipher items component

--- src/app/components/ciphers/ciphers.component.ts
+++ src/app/components/ciphers/ciphers.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Cipher } from "../../models/Cipher";
 
 @Component({
-  selector: 'app-ciphers',
-  templateUrl: './ciphers.component.html',
-  styleUrls: ['./ciphers.component.css']
+  selector: "app-ciphers",
+  templateUrl: "./ciphers.component.html",
+  styleUrls: ["./ciphers.component.css"]
 })
 export class CiphersComponent implements OnInit {
+  ciphers: Cipher[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.ciphers = [
+      {
+        toShift: "I love node!",
+        afterShift: "E hkra jkza!",
+        shiftAmount: 22
+      }
+    ];
   }
-
 }
