985817e8983c478d1cad5f19d6419d3de04107dc
Russ yev <ruslanyev@yahoo.com>
2019-07-17 23:08:11 -0700

    removed angular references in the html and updated name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client-caesar-cipher';
+  title:string = 'Client Caesar Cipher';
 }
